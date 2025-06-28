5eb36c503b4cee5195d029afc42b15c8b9da16ed
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-06-29 00:54:58 +0530

    fix: test file naming convention

--- src/__test__/page.test.jsx
+++ src/__test__/page.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { test, expect } from 'vitest'
-import Page from '../app/Page'
+import Page from '../app/page'
 
 test('Page', () => {
   render(<Page />)
